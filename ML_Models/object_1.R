2fa5c563b08597ada66d7ad19f2f03e883dd6f71
Banuji Liyanwala <banuji01liyanwala@gmail.com>
2023-04-28 01:43:03 +0530

    transform new data set using PCA & bug fixed

--- ML_Models/object_1.R
+++ ML_Models/object_1.R
@@ -24,8 +24,6 @@ scaled_vehicles <- scaled_vehicles[-outliers, ]
 # Determine the optimal number of clusters using NbClust
 nbclust_index <- NbClust(scaled_vehicles, min.nc = 2, max.nc = 10, method = "kmeans", index = "all")
 
-# Display the results
-print(nbclust_index)
 
 # Plot the elbow curve
 set.seed(123)
@@ -34,18 +32,18 @@ for (i in 1:10) {
   km <- kmeans(scaled_vehicles, centers = i, nstart = 10)
   wss[i] <- sum(km$withinss)
 }
-plot(wss, type = "b", pch = 19, frame = FALSE, 
-     xlab = "Number of clusters", ylab = "WSS", 
+plot(wss, type = "b", pch = 19, frame = FALSE,
+     xlab = "Number of clusters", ylab = "WSS",
      main = "Elbow Method")
 
 # Find the optimal number of clusters using elbow method
 elbow_index <- which(diff(wss) < mean(diff(wss)))[1] + 2
 cat("Optimal number of clusters using elbow method: ", elbow_index, "\n")
 
-# Compute and plot the gap statistic with number of clusters 
+# Compute and plot the gap statistic with number of clusters
 set.seed(123)
-gap_stat <- clusGap(scaled_vehicles, FUN = kmeans, 
-                    K.max = 2, B = 50, verbose = FALSE, 
+gap_stat <- clusGap(scaled_vehicles, FUN = kmeans,
+                    K.max = 2, B = 50, verbose = FALSE,
                     nstart = 10)
 plot(gap_stat, main = "Gap Statistic", xlab = "Number of clusters")
 
@@ -53,20 +51,43 @@ plot(gap_stat, main = "Gap Statistic", xlab = "Number of clusters")
 gap_index <- which.max(gap_stat$Tab[, "gap"]) + 1
 cat("Optimal number of clusters using gap statistic: ", gap_index, "\n")
 
-# Compute and plot the silhouette widths
-set.seed(123)
-sil_width <- c()
-for (i in 2:10) {
-  km <- kmeans(scaled_vehicles, centers = i, nstart = 10)
-  sil_width[i] <- mean(silhouette(km$cluster, dist(scaled_vehicles)))
-}
-plot(sil_width, type = "b", pch = 19, frame = FALSE, 
-     xlab = "Number of clusters", ylab = "Silhouette Width", 
-     main = "Silhouette Method")
-cat("Optimal number of clusters using silhouette method: ", which.max(sil_width), "\n")
+# Calculate the average silhouette width for different values of k
 
-# Set the optimal number of clusters
-k <- gap_index
+k.min <- 2
+k.max <- 10
+
+# Create a list to store the silhouette values for each value of K
+silhouette_vals <- vector("list", k.max - k.min + 1)
+
+# Loop through each value of K and perform clustering using K-means algorithm
+for (k in k.min:k.max) {
+  km <- kmeans(scaled_vehicles, centers = k, nstart = 10)
+
+  # Calculate the silhouette width for each data point
+  silhouette_vals[[k - k.min + 1]] <- silhouette(km$cluster, dist(scaled_vehicles))
+}
+
+# Calculate the average silhouette width for each value of K
+silhouette_avg <- sapply(silhouette_vals, function(x) mean(x[, 3]))
+
+# Create a data frame with the silhouette widths for each value of K
+df <- data.frame(k = k.min:k.max, silhouette = silhouette_avg)
+
+# Plot the silhouette widths using ggplot2
+ggplot(df, aes(x = k, y = silhouette)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Number of clusters", y = "Silhouette")
+
+# Find the index of the maximum silhouette width
+best_k <- which.max(silhouette_avg) + k.min - 1
+
+# Print the best number of clusters based on the silhouette method
+cat("Best number of clusters based on the silhouette method: ", best_k, "\n")
+
+
+# Set the optimal number of clusters using the best method (elbow) from the above
+k <- elbow_index
 
 # Perform k-means clustering
 set.seed(123)
@@ -90,9 +111,6 @@ cat("Within-cluster sum of squares: ", WSS, "\n")
 # Plot the cluster results
 fviz_cluster(kmeans_model, data = scaled_vehicles)
 
-
-
-
 # Compute the silhouette widths
 silhou_width <- silhouette(kmeans_model$cluster, dist(scaled_vehicles))
 
@@ -101,13 +119,10 @@ plot(silhou_width, main = "Silhouette Plot for K-Means Clustering")
 
 # Plot the silhouette plot in a new page
 pdf("silhouette_plot.pdf")
-plot(silhou_width, type = "b", pch = 19, frame = FALSE, 
-     xlab = "Number of clusters", ylab = "Silhouette Width", 
+plot(silhou_width, type = "b", pch = 19, frame = FALSE,
+     xlab = "Number of clusters", ylab = "Silhouette Width",
      main = "Silhouette Method")
 dev.off()
-
-# Display the optimal number of clusters using silhouette method
-cat("Optimal number of clusters using silhouette method: ", which.max(sil_width), "\n")
 
 # Compute the silhouette widths
 silhou_width <- silhouette(kmeans_model$cluster, dist(scaled_vehicles))
@@ -117,53 +132,26 @@ plot(silhou_width, main = "Silhouette Plot for K-Means Clustering")
 avg_sil_width <- summary(silhou_width)$avg.width
 cat("Average Silhouette Width: ", avg_sil_width, "\n")
 
+#second sub task - clustering object
 
-# Load the iris dataset
-data(iris)
+# Perform PCA on the scaled data
+pca_vehicles <- prcomp(scaled_vehicles, center = TRUE, scale. = TRUE)
 
-# Extract the numerical variables
-iris_num <- iris[, 1:4]
+# View eigenvalues and eigenvectors
+print(pca_vehicles)
 
-# Create the data matrix
-data_matrix <- as.matrix(iris_num)
+# View cumulative scores per principal component
+plot(pca_vehicles)
 
-pca_res <- prcomp(data_matrix)
+# Create a new dataset with principal components as attributes
+transformed_vehicles <- as.data.frame(predict(pca_vehicles))
 
-# Check number of columns in pca_res$ind$coord
-if (ncol(pca_res$ind$coord) < 6) {
-  # If number of columns is less than 6, use all columns
-  coord_cols <- 1:ncol(pca_res$ind$coord)
-} else {
-  # If number of columns is greater than or equal to 6, use first 6 columns
-  coord_cols <- 1:6
-}
+# Choose principal components that provide at least 92% cumulative score
+cumulative_scores <- cumsum(pca_vehicles$sdev^2 / sum(pca_vehicles$sdev^2))
+num_components <- sum(cumulative_scores <= 0.92)
+chosen_components <- paste0("PC", 1:num_components)
+transformed_vehicles <- transformed_vehicles[, chosen_components]
 
-# Extract PCA coordinates for first 6 components
-pca_coords <- pca_res$ind$coord[, coord_cols]
-
-
-# Perform PCA analysis
-pca_res <- PCA(scaled_vehicles, graph = FALSE)
-# Perform PCA
-
-# Display the eigenvalues and eigenvectors
-print(pca_res$eig)
-print(pca_res$var$contrib)
-
-# Plot the cumulative score per principal components
-fviz_eig(pca_res, addlabels = TRUE, ylim = c(0, 100))
-
-# Create a transformed dataset with principal components as attributes
-transformed_vehicles <- as.data.frame(pca_res$ind$coord[, 1:6])
-
-# Choose those PCs that provide at least cumulative score > 92%
-pc_cumsum <- cumsum(pca_res$eig$percent)
-chosen_pcs <- which(pc_cumsum > 92)
-
-# Provide a brief discussion for your choice to choose specific number of PCs
-cat("Cumulative percentage of variance explained by each PC:\n")
-print(pc_cumsum)
-cat("\n")
-cat("Number of PCs needed to explain at least 92% of the variance: ", length(chosen_pcs), "\n")
-
+# Determine the optimal number of clusters using NbClust
+nbclust_index <- NbClust(transformed_vehicles, min.nc = 2, max.nc = 10, method = "kmeans", index = "all")
 
